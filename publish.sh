f640d1a2a20fd7a5bb90a51ea3262d702bf92f75
Marcos Barbero <marcos.hgb@gmail.com>
2018-02-13 20:20:56 +0100

    Tue Feb 13 20:20:56 CET 2018

--- publish.sh
+++ publish.sh
@@ -1,12 +1,17 @@
 rm -rf _site/
-jekyll build
+bundle exec jekyll build
+
+git fetch --all
+git push origin --delete master
+git branch -D master
+
+git checkout develop
 
 cp CNAME _site/CNAME
 
-git branch -D master
-git checkout develop
+git add --all
+git commit -m "`date`"
 
 git subtree split --prefix _site -b master
-git push -f origin master:master
 
-git branch -D master
+git push -f origin master:master
