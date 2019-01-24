0bd6b809697eb1f98a712c1d54bca1fd8ccd0cc0
Marcos Barbero <marcos.hgb@gmail.com>
2019-01-24 18:55:10 +0100

    Play with origin [ci deploy]

--- publish.sh
+++ publish.sh
@@ -2,14 +2,13 @@ setup_git() {
    git config --global user.email "travis@travis-ci.org"
    git config --global user.name "Travis CI"
    git remote rm origin
-   git remote add origin https://${GH_TOKEN}@github.com/marcosbarbero/marcosbarbero.github.io.git
+   git remote add origin https://$GH_TOKEN@github.com/marcosbarbero/marcosbarbero.github.io.git
 }
 
 build_site() {
 	rm -rf _site/
 	bundle install
     bundle exec jekyll build
-    ls -lah _site/
 }
 
 fetch_n_checkout_branch() {
@@ -26,7 +25,7 @@ commit_n_push() {
 
     git subtree split --prefix _site -b master
     # git push --quiet --set-upstream -f origin-pages master
-    git push -f origin master
+    git push -f origin master:master
 }
 
 if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
