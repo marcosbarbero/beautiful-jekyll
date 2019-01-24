c0f411225db6625b0b3289b7c1080cedc8f56319
Marcos Barbero <marcos.hgb@gmail.com>
2019-01-24 14:28:01 +0100

    Add travis.yml [ci deploy]

--- publish.sh
+++ publish.sh
@@ -1,17 +1,39 @@
-rm -rf _site/
-bundle exec jekyll build
+setup_git() {
+   git config --global user.email "travis@travis-ci.org"
+   git config --global user.name "Travis CI"
+   git remote add origin-pages https://${GH_TOKEN}@github.com/marcosbarbero/marcosbarbero.github.io.git > /dev/null 2>&1
+}
 
-git fetch --all
-# git push origin --delete master
-git branch -D master
+build_site() {
+	rm -rf _site/
+    bundle exec jekyll build
+}
 
-git checkout develop
+fetch_n_checkout_branch() {
+	git fetch --all
+    # git push origin --delete master
+    git branch -D master
 
-cp CNAME _site/CNAME
+    git checkout develop
+}
 
-git add --all
-git commit -m "`date`"
+commit_n_push() {
+	git add --all
+    git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-git subtree split --prefix _site -b master
+    git subtree split --prefix _site -b master
+    git push --quiet --set-upstream -f origin-pages master
+}
 
-git push -f origin master:master
+if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] && [[ "$TRAVIS_COMMIT_MESSAGE" == *"[ci deploy]"* ]]; then
+
+	setup_git
+	build_site
+	fetch_n_checkout_branch   
+
+    cp CNAME _site/CNAME
+
+    commit_n_push
+
+    echo "New version released"
+fi 
